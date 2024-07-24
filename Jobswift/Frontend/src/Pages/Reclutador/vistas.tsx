dc9ce8397eb8d9c9f4432cffbd9ff909273a42aa
joaquinMatos <balamema10@gmail.com>
2024-07-24 17:19:59 -0500

    De: completo

--- Jobswift/Frontend/src/Pages/Reclutador/vistas.tsx
+++ Jobswift/Frontend/src/Pages/Reclutador/vistas.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import { Box, Typography, Paper, Grid, Button } from "@mui/material";
+import { Box, Typography, Grid, Button } from "@mui/material";
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
 
 const BienvenidoReclutador = () => {
     return (
-        <Box sx={{ bgcolor: '#E3F2FD', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <Paper elevation={3} sx={{ padding: '40px', borderRadius: '12px', textAlign: 'center' }}>
-                <Typography variant="h3" component="div" sx={{ mb: 3 }}>
-                    ¡Bienvenido, Reclutador!
-                </Typography>
-                <Typography variant="body1" sx={{ mb: 3 }}>
-                    Estamos encantados de tenerte aquí. Utiliza este panel para gestionar tus ofertas de trabajo, 
-                    buscar candidatos y más.
-                </Typography>
-                <Grid container spacing={2} justifyContent="center">
-                    <Grid item>
-                        <Button variant="contained" color="primary" href="/crear-oferta">
-                            Crear Nueva Oferta
-                        </Button>
-                    </Grid>
-                    <Grid item>
-                        <Button variant="contained" color="secondary" href="/buscar-candidatos">
-                            Buscar Candidatos
-                        </Button>
-                    </Grid>
+        <Box sx={{ bgcolor: '#E3F2FD', minHeight: '95vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', textAlign: 'center' }}>
+            <WorkOutlineIcon sx={{ fontSize: 270, color: '#007AFF', mb: 3 }} />
+            <Typography variant="h3" component="div" sx={{ mb: 3 }}>
+                ¡Bienvenido, Reclutador!
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 3 }}>
+                Estamos encantados de tenerte aquí. Utiliza este panel para gestionar tus ofertas de trabajo, 
+                buscar candidatos y más.
+            </Typography>
+            <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                    <Button variant="contained" color="primary" href="/crear-oferta">
+                        Crear Nueva Oferta
+                    </Button>
                 </Grid>
-            </Paper>
+                <Grid item>
+                    <Button variant="contained" color="info" href="/buscar-candidatos">
+                        Buscar Candidatos
+                    </Button>
+                </Grid>
+            </Grid>
         </Box>
     );
 }
--- Jobswift/backend/Domain/obj/Debug/net6.0/Domain.AssemblyInfo.cs
+++ Jobswift/backend/Domain/obj/Debug/net6.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37b29b330e7ce0e62e76c5f81cfe4277e8f05420")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb6477a89ddc60b9be6a95c6914eae806dcb58ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
