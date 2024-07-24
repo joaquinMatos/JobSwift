9f071d86f0ff601876d1e97bba2508c0c32e4a27
joaquinMatos <balamema10@gmail.com>
2024-07-23 20:28:28 -0500

    hola

--- Jobswift/backend/backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
+++ Jobswift/backend/backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76745210c93e060cbf955eb04300cf141ab3b8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9298c0c6066e587a897c34240f6aa6e882a05b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
