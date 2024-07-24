aede94e78b8fa78867c22ef117f58ee4c6925121
Larry Basto <larrybasto72@gmail.com>
2024-07-24 17:41:35 -0500

    api

--- Jobswift/Frontend/src/Pages/Reclutador/vistas.tsx
+++ Jobswift/Frontend/src/Pages/Reclutador/vistas.tsx
@@ -1,8 +1,10 @@
+// src/components/BienvenidoReclutador.tsx
 import React from 'react';
 import { Box, Typography, Grid, Button } from "@mui/material";
 import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import News from '../../components/News';
 
-const BienvenidoReclutador = () => {
+const BienvenidoReclutador: React.FC = () => {
     return (
         <Box sx={{ bgcolor: '#E3F2FD', minHeight: '95vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', textAlign: 'center' }}>
             <WorkOutlineIcon sx={{ fontSize: 270, color: '#007AFF', mb: 3 }} />
@@ -13,18 +15,8 @@ const BienvenidoReclutador = () => {
                 Estamos encantados de tenerte aquí. Utiliza este panel para gestionar tus ofertas de trabajo, 
                 buscar candidatos y más.
             </Typography>
-            <Grid container spacing={2} justifyContent="center">
-                <Grid item>
-                    <Button variant="contained" color="primary" href="/crear-oferta">
-                        Crear Nueva Oferta
-                    </Button>
-                </Grid>
-                <Grid item>
-                    <Button variant="contained" color="info" href="/buscar-candidatos">
-                        Buscar Candidatos
-                    </Button>
-                </Grid>
-            </Grid>
+            
+            <News />
         </Box>
     );
 }
